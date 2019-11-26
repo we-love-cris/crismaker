73db8b1c7f8dde7a80054bf712e9cae7198ebd96
flydog98 <pyjpyj0825@gmail.com>
2019-11-26 16:22:26 +0900

    초기화 관련 함수들 init.h에 선언

--- init.h
+++ init.h
@@ -5,6 +5,12 @@
 #include "declare.h" //기본 라이브러리 및, 상수값, 구조체 include
 #include "util.h" //util 함수들 include
 
-void init_status(status* now_game) {
-	//모든 기본적인 값들에 대한 초기화
-}
+//모든 기본적인 값들에 대한 초기화
+void init_status(status* game);
+
+
+void make_status(status* game);
+void make_student(student* std);
+void make_player_p(player_p* choi);
+void make_prof(prof* pp);
+void make_day(day* wk);

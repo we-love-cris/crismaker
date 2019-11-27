0fd3e29ab381a94c794d085607e6d5f6edf88c28
flydog98 <pyjpyj0825@gmail.com>
2019-11-27 11:40:05 +0900

    init 추가 및 구조체 선언 순서 변경

--- init.c
+++ init.c
@@ -1,2 +1,44 @@
 #include "init.h"
 
+void init_status(status* game) {
+
+}
+
+
+void make_status(status* game) {
+
+}
+void make_student(student* std) {
+	std->attend_count = 0;
+	std->attend_rate = 0;
+	std->happiness = 0;
+	std->move = 0;
+	std->progress = 0;
+	std->skill = 0;
+	std->tired = 0;
+	std->type = 2;
+
+	std->name = "";
+}
+void make_player_p(player_p* choi) {
+	choi->assignment = 0;
+	choi->chat = 0;
+	choi->move = 0;
+	choi->progress = 0;
+	choi->test = 0;
+	choi->type = 0;
+
+	choi->name = "";
+}
+void make_prof(prof* pp) {
+	pp->chat_rate = 0;
+	pp->progress_rate = 0;
+	pp->type = 0;
+
+	pp->name = "";
+}
+void make_day(day* wk) {
+	wk->attend_rate = 0;
+	wk->move = 0;
+	wk->type = 0;
+}

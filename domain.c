34470d926241b386b16e0e37a749f7bc2c216f13
Yongjun Park <50614833+flydog98@users.noreply.github.com>
2019-12-01 19:53:55 +0900

    메뉴 선택 추가

--- domain.c
+++ domain.c
@@ -1,2 +1,30 @@
 #include "domain.h"
 
+int main_menu() {
+	int ret = 0;
+	v_menusquare();
+	v_main_menu();
+	ret = choose_menu(4);
+
+	return ret;
+}
+
+int choose_menu(int max) {
+	int chosen = 1;
+	int get = 0;
+	v_menu_indi(chosen, "▶");
+	while (get != ENTER) {
+		get = get_wasd();
+		v_menu_indi(chosen, "  ");
+		if (chosen < max && (get == DOWN || get == RIGHT)) {//아래나 오른쪽 누르면
+			chosen++;//선택메뉴 증가
+		}
+		else if (chosen > 1 && (get == UP || get == LEFT)) {//위나 왼쪽 누르면
+			chosen--;//선택메뉴 감소
+		}
+		v_menu_indi(chosen, "▶");
+	}
+	v_menu_indi(chosen, "  ");
+
+	return chosen;
+}

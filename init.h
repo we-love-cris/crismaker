6e48b74eba40b8b1b9b555c659d103954c375727
Yongjun Park <50614833+flydog98@users.noreply.github.com>
2019-12-01 19:55:09 +0900

    콘솔 관련 init추가

--- init.h
+++ init.h
@@ -3,6 +3,7 @@
 
 #include "declare.h" //기본 라이브러리 및, 상수값, 구조체 include
 #include "util.h" //util 함수들 include
+#include "view.h"
 
 //모든 기본적인 값들에 대한 초기화
 status* init_status();
@@ -13,3 +14,7 @@ void make_student(student* std);
 void make_player_p(player_p* choi);
 void make_prof(prof* pp);
 void make_day(day* wk);
+
+
+//콘솔창 초기화
+void init_consol();

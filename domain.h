573303ebb9127079399eae892255e263c490059d
Yongjun Park <50614833+flydog98@users.noreply.github.com>
2019-12-01 22:40:28 +0900

    메인 메뉴와 교수학생 모드 선택 추가

--- domain.h
+++ domain.h
@@ -4,8 +4,11 @@
 #include "util.h"
 #include "view.h"
 
-//메인메뉴 실행, 선택한 항목을 리턴한다
-int main_menu();
+//선택한 항목을 리턴한다, 메인 메뉴와 교수학생 선택에 쓰인다
+//amount만큼의 항목을 선택할 수 있다
+int select_main_menu(int amount);
+int select_profvsstd(int amount);
+
 
 //1~max까지의 메뉴를 선택하는 함수
 int choose_menu(int max);

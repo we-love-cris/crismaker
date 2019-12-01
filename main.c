6b2e5a0f6d24ca695fd5a421cc5b94f91c450cad
Yongjun Park <50614833+flydog98@users.noreply.github.com>
2019-12-01 22:42:07 +0900

    메인 모드  흐름 편집 중

--- main.c
+++ main.c
@@ -6,15 +6,39 @@
 
 
 int main(void) {
-	init_consol();
 	//선언 및 초기화
+	int life = 1;
 	int menu_select = 0;
+	int game_mode = 0;
+
 	status* game = init_status();
+	init_consol(game);
+
 	//게임 시작 화면(press any key)
 	v_start();
 	//메인 메뉴
-	while (1) {
-		menu_select = main_menu();
+	while (life) {
+		menu_screen();
+		menu_select = game->func_list.mp[0](4);
+
+		switch (menu_select) {
+		case 1://게임 시작, 모드 선택
+			menu_screen();
+			game_mode = game->func_list.mp[1](2);
+			break;
+		case 2://학사일정 편집
+			game_screen();
+			break;
+		case 3://시험문제 수정
+			game_screen();
+			break;
+		case 4://게임 종료
+			life = 0;
+			break;
+		default:
+			life = 0;
+			break;
+		}
 		//교수 및 학생 모드 입력받기
 
 		//이름 입력받기
@@ -32,6 +56,6 @@ int main(void) {
 
 
 	}
-
+	gotoxy(0, 0);
 	return 0;
 }

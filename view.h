bc8612070687d6cb2e95d7c0040dbbf92d4b8873
Yongjun Park <50614833+flydog98@users.noreply.github.com>
2019-12-01 22:44:16 +0900

    화면 크기에 맞춰 함수들 수정 및 clear계열 함수 추가

--- view.h
+++ view.h
@@ -7,7 +7,11 @@
 void v_start();
 
 //메뉴 선택
+//4가지 메인 메뉴를 나타낸다
 void v_main_menu();
+//학생, 교수를 선택하는 메뉴
+void v_profvsstd();
+//▶를 표시해주는 함수
 void v_menu_indi(int chosen, char ch[3]);
 
 //화면 틀
@@ -17,6 +21,7 @@ void v_menusquare();
 void v_board();
 
 //기능 함수들
-void v_row(int height);
+void v_row(int height, int amount);
 void v_ver(int start, int end, int height, int amount);
+void v_clear_menusquare();
 void v_clearall();

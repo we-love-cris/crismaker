e1fe4adf3f4420ecc7823293ded06a5bc7fa585a
kth990303 <57135043+kth990303@users.noreply.github.com>
2019-12-05 09:59:11 +0900

    이름 할당 및 화면 넓히기 추가
    출석부 이름 할당 (과정 중 창 y축 크기 36으로 넓힘)
    main.c에서 line 28 밑에 stdnaming(game); 추가해주기.

---
+++ studentinfo.c
@@ -0,0 +1,43 @@
+#include "constants.h"
+#include "init.h"
+#include "declare.h"
+
+void allocname(status *game, char * name) {
+	int i, len = 0;
+	
+	//name 동적할당 해주고 unit.c에 넣어주기
+	//현재 이름이 두 줄을 초과하는 경우는 에러 발생.
+	for (i = 0; i < STD_AMOUNT; i++) {
+		game->student_list[i]->name = (char *)malloc(sizeof(char) * strlen(name));
+		if (game->student_list[i]->name == NULL) {
+			printf("Error: 이보다 많은 학생 수는 수업을 듣길 원치 않습니다.\n");
+			exit(1);
+		}
+	}
+
+}
+void stdnaming(status *game) {
+	int i, len = 0;
+	int y = 3;
+	char name[12] = "";
+
+	system("mode con: cols=44 lines=36");
+
+	student** student_list = (student**)calloc(0x0, sizeof(student*) * 7);
+	if (student_list == NULL) {
+		printf("학생 정보 입력 중 에러 발생! \n");
+		printf("Error: 학생 정보창을 불러일으킬 수 없습니다.\n");
+		exit(1);
+	}
+
+	for (i = 0; i < STD_AMOUNT; i++) {
+		//출석부랑 연동시켜서 1~12번째 이름 입력받게 하고
+		//gotoxy(int x, int y)
+		gotoxy(3, y); printf("%d 번째 학생 --> 이름 입력: ", i + 1);
+		scanf("%s", name); 
+		allocname(game, name);
+		//이름을 학생 정보에 연동시켜서 저장해야 함.
+		strcpy(game->student_list[i]->name, name);
+		y = y + 2;
+	}
+}

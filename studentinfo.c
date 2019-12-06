305aa30ef428267ed0bfa9016acf42a3f4a14988
TaeHyeon Kim <57135043+kth990303@users.noreply.github.com>
2019-12-06 11:05:30 +0900

    디버깅 과정 후 이름 할당 수정

--- studentinfo.c
+++ studentinfo.c
@@ -7,6 +7,7 @@ void allocname(status *game, char * name) {
 
 	for (i = 0; i < STD_AMOUNT; i++) {
 		game->student_list[i]->name = (char *)realloc(game->student_list[i]->name, sizeof(char) * strlen(name));
+		strcpy(game->student_list[i]->name, name);
 		if (game->student_list[i]->name == NULL) {
 			printf("Error: 이보다 많은 학생 수는 수업을 듣길 원치 않습니다.\n");
 			exit(1);
@@ -32,7 +33,6 @@ void stdnaming(status *game) {
 		//출석부랑 연동시켜서 1~12번째 이름 입력받게 하고
 		gotoxy(3, y); printf("%d 번째 학생 --> 이름 입력: ", i + 1);
 		scanf("%s", name);
-		
 		allocname(game, name);
 		//이름을 학생 정보에 연동시켜서 저장해야 함.
 		strcpy(game->student_list[i]->name, name);
